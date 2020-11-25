62b6592e56dbd64449965ea77df7c78257db83bb
[vahermihkel] <[vahermihkel@gmail.com]>
2020-11-25 20:06:38 +0200

    portfolio main page

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { WorkComponent } from './work/work.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    WorkComponent,
+    HobbiesComponent
   ],
   imports: [
     BrowserModule,
