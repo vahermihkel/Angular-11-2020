29bfc6ab8fa7bc1673e797b374e92ed07f975233
[vahermihkel] <[vahermihkel@gmail.com]>
2020-11-30 20:03:46 +0200

    portfoolio leht

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { WorkComponent } from './work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "kursused", component: CoursesComponent },
+  { path: "hobid", component: HobbiesComponent },
+  { path: "work", component: WorkComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
